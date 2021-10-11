16554a689a7ffe0ea5da72c36cd54a8a0f077511
vaibhav190651 <80534832+vaibhav190651@users.noreply.github.com>
2021-10-11 16:42:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(RIGHT_ARROW)){
+   // box.velocityX = 3;
+   box.x=box.x +5;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box.x=box.x -5;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    box.y=box.y -5;
+  }
+
+
+  if(keyIsDown(DOWN_ARROW)){
+     box.y=box.y +5;
+  }
+
+  drawSprites();
+
+}
+
+
+
+
